ff5ef334638d5c3a643bd802f39d7dea72d163bd
Guto Maia <guto@guto.net>
2011-12-14 15:24:53 -0200

    some tf2 tests are done, more are needed before refactoring

--- src/test/javascript/tf2stats_test.js
+++ src/test/javascript/tf2stats_test.js
@@ -17,3 +17,8 @@ assert.equal(3, stats.visibilityState);
 assert.equal("TF2", stats.gameFriendlyName);
 assert.equal("Team Fortress 2", stats.gameName);
 assert.equal("http://store.steampowered.com/app/440", stats.gameLink);
+assert.equal("http://media.steampowered.com/steamcommunity/public/images/apps/440/e3f595a92552da3d664ad00277fad2107345f743.jpg", stats.gameIcon);
+assert.equal("http://media.steampowered.com/steamcommunity/public/images/apps/440/07385eb55b5ba974aebbe74d3c99626bda7920b8.jpg", stats.logo);
+assert.equal("http://media.steampowered.com/steamcommunity/public/images/apps/440/07385eb55b5ba974aebbe74d3c99626bda7920b8_thumb.jpg", stats.logoSmall);
+assert.equal(76561197985077150,stats.steamID64);
+assert.equal("gutomaia",stats.playerCustomURL);
