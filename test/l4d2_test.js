90d44c50ff436b3d8e579b8380da0af556e48d29
Guto Maia <guto@guto.net>
2011-12-16 18:18:31 -0200

    refactoring l4d2_test to follow nodeunit standard

--- test/l4d2_test.js
+++ test/l4d2_test.js
@@ -1,8 +1,6 @@
 var fs = require('fs');
 var sys = require('util');
 
-var assert = require('assert');
-
 var tf2status_function = fs.readFileSync(__dirname + '/../src/tf2stats.js', 'utf8');
 
 eval(tf2status_function); //TODO need to change the filename
@@ -13,12 +11,49 @@ var statsClient = new StatsClient();
 
 var stats = statsClient.getStats('gutomaia', 'l4d2', tf2status_xml);
 
-assert.equal(3, stats.visibilityState);
-assert.equal("L4D2", stats.gameFriendlyName);
-assert.equal("Left 4 Dead 2", stats.gameName);
-assert.equal("http://store.steampowered.com/app/550", stats.gameLink);
-assert.equal("http://media.steampowered.com/steamcommunity/public/images/apps/550/7d5a243f9500d2f8467312822f8af2a2928777ed.jpg", stats.gameIcon);
-assert.equal("http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142.jpg", stats.logo);
-assert.equal("http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142_thumb.jpg", stats.logoSmall),
-assert.equal(76561197985077150, stats.steamID64);
-assert.equal("gutomaia", stats.playerCustomURL);
+exports.testVisibilityState = function(test){
+    test.equal(3, stats.visibilityState);
+    test.done();
+}
+
+exports.testGameFriendlyName = function(test){
+    test.equal("L4D2", stats.gameFriendlyName);
+    test.done();
+}
+
+exports.testGameName = function(test){
+    test.equal("Left 4 Dead 2", stats.gameName);
+    test.done();
+}
+
+exports.testGameLink = function(test){
+    test.equal("http://store.steampowered.com/app/550", stats.gameLink);
+    test.done();
+}
+
+exports.testGameIcon = function (test){
+    test.equal("http://media.steampowered.com/steamcommunity/public/images/apps/550/7d5a243f9500d2f8467312822f8af2a2928777ed.jpg", stats.gameIcon);
+    test.done();
+}
+
+exports.testGameLogo = function (test){
+    test.equal("http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142.jpg", stats.logo);
+    test.done();
+}
+
+exports.testGameLogoSmall = function (test){
+    test.equal("http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142_thumb.jpg", stats.logoSmall);
+    test.done();
+}
+
+exports.testSteamID64 = function (test){
+    test.equal(76561197985077150, stats.steamID64);
+    test.done();
+}
+
+exports.testPlayerCustomURL = function (test){
+    test.equal("gutomaia", stats.playerCustomURL);
+    test.done();
+}
+
+
